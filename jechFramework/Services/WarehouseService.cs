5ecba2e30fd8d31be8c421cc4486a56aa3cc6b1d
hannan0912 <hannan.s.1999@gmail.com>
2024-01-24 12:11:44 +0100

    satt på plass de forskjellige filene

--- jechFramework/Services/WarehouseService.cs
+++ jechFramework/Services/WarehouseService.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace jechFramework
+namespace jechFramework.Services
 {
-    internal interface Interface1
+    internal class WarehouseService
     {
-
     }
 }
