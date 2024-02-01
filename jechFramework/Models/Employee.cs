f31142284e238cf608d82158926a97fb54114a8a
Christopher Fredh <88053773+Chrizziboi@users.noreply.github.com>
2024-02-01 17:31:43 +0100

    added the employee class

---
+++ jechFramework/Models/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jechFramework.Models
+{
+    internal class Employee
+    {
+        public int employeeId 
+        { get; set; }
+        public string employeeName 
+        { get; set; }
+        public string employeeDescription
+        { get; set; }
+        public int employeeAge 
+        { get; set; }
+        public int employeePersonalId 
+        { get; set; }
+        public string employeeAddress 
+        { get; set; }
+        public string employeeCity 
+        { get; set; }
+        public string employeeTelephoneNumber 
+        { get; set; }
+
+
+        public Employee(int employeeId, string employeeName, string employeeDescription,
+            int employeeAge, int empployeePersonalId, string employeeAddress, string employeeCity,
+            string employeeTelephoneNumber) 
+        {
+            this.employeeId = employeeId;
+            this.employeeName = employeeName;
+            this.employeeDescription = employeeDescription;
+            this.employeeAge = employeeAge;
+            this.employeePersonalId = empployeePersonalId;
+            this.employeeAddress = employeeAddress;
+            this.employeeCity = employeeCity;
+            this.employeeTelephoneNumber = employeeTelephoneNumber;
+
+        }
+
+    }
+}
