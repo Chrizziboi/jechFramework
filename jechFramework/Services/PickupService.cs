570393b39b223891174745a33ab7bfa7ac86b332
hannan0912 <hannan.s.1999@gmail.com>
2024-02-01 15:57:51 +0100

    implementert metodene

--- jechFramework/Services/PickupService.cs
+++ jechFramework/Services/PickupService.cs
@@ -2,34 +2,34 @@
 using jechFramework.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace jechFramework.Services
 {
     public class PickupService : IPickupService
     {
+        private readonly List<Pickup> _scheduledPickups = new List<Pickup>();
+        private readonly List<RecurringPickup> _scheduledRecurringPickups = new List<RecurringPickup>();
+
         /// <summary>
         /// Planlegger en enkeltgangshenting av varer fra lageret på et spesifisert tidspunkt.
-        /// Denne metoden vil, når den implementeres, registrere detaljene for en planlagt henting,
-        /// inkludert hvilke varer som skal hentes og når.
         /// </summary>
         /// <param name="pickup">Inneholder detaljer om hentingen som skal planlegges.</param>
         public void SchedulePickup(Pickup pickup)
         {
-            // Tom implementasjon for nå.
+            // Her kan du legge til logikk for å validere pickup-detaljer.
+            _scheduledPickups.Add(pickup);
+            // Implementer funksjonalitet for å faktisk planlegge hentingen.
         }
 
         /// <summary>
         /// Planlegger gjentagende hentinger av varer fra lageret basert på et definert mønster (f.eks. daglig eller ukentlig).
-        /// Denne metoden vil håndtere oppsettet for regelmessige hentinger,
-        /// og sikre at disse hentinger skjer i henhold til det spesifiserte mønsteret.
         /// </summary>
         /// <param name="recurringPickup">Inneholder detaljer om den gjentagende hentingen som skal planlegges.</param>
         public void ScheduleRecurringPickup(RecurringPickup recurringPickup)
         {
-            // Tom implementasjon for nå.
+            // Her kan du legge til logikk for å validere recurringPickup-detaljer.
+            _scheduledRecurringPickups.Add(recurringPickup);
+            // Implementer funksjonalitet for å håndtere gjentagende hentinger.
         }
     }
 }
