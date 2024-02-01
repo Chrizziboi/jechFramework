8ec662d4526651411268bc57f9849be133bd351c
hannan0912 <hannan.s.1999@gmail.com>
2024-02-01 17:47:03 +0100

    endra navn på variabel siden Christopher ikke var fornøyd :)

---
+++ jechFramework/Interfaces/IWaresInService.cs
@@ -0,0 +1,33 @@
+﻿using jechFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jechFramework.Interfaces
+{
+    /// <summary>
+    /// Definerer tjenester for planlegging av mottak av varer til lageret.
+    /// </summary>
+    public interface IWaresInService
+    {
+        /// <summary>
+        /// Planlegger en enkeltgangs mottak av varer til lageret.
+        /// Denne metoden skal, når den implementeres, håndtere logikken for å registrere
+        /// og organisere mottak av en vare på et spesifikt tidspunkt.
+        /// </summary>
+        /// <param name="waresIn">Detaljer om varemottaket som skal skje.</param>
+        void ScheduleWaresIn(WaresIn waresIn);
+
+        /// <summary>
+        /// Planlegger gjentagende mottak av varer til lageret.
+        /// Denne metoden skal, når implementert, håndtere oppsettet av regelmessige mottak
+        /// basert på et definert mønster (som daglig eller ukentlig).
+        /// </summary>
+        /// <param name="recurringOrder">Detaljer om den gjentagende varemottaket som skal planlegges.</param>
+        void ScheduleRecurringOrder(RecurringOrder recurringOrder);
+
+        // Eventuelle andre nødvendige metoder relatert til mottak av varer...
+    }
+}
--- jechFramework/Models/WaresIn.cs
+++ jechFramework/Models/WaresIn.cs
@@ -7,34 +7,34 @@ using System.Threading.Tasks;
 namespace jechFramework.Models
 {
     /// <summary>
-    /// Representerer en enkeltgangshenting av en vare fra lageret.
-    /// Inneholder all nødvendig informasjon for å identifisere og planlegge hentingen.
+    /// Representerer en enkeltgangs innkomst av varer til lageret.
+    /// Inneholder all nødvendig informasjon for å identifisere og planlegge mottak av varer.
     /// </summary>
-    public class Pickup
+    public class WaresIn
     {
-        public int PickupId { get; set; }
+        public int OrderId { get; set; }
         public int InternalId { get; set; }
         public DateTime ScheduledTime { get; set; }
     }
 
     /// <summary>
-    /// Representerer en plan for gjentagende hentinger av varer fra lageret.
+    /// Representerer en plan for gjentagende mottak av varer til lageret.
     /// Inkluderer starttidspunkt og mønsteret for gjentakelse (f.eks., daglig eller ukentlig).
     /// </summary>
-    public class RecurringPickup
+    public class RecurringOrder
     {
-        public int PickupId { get; set; }
+        public int OrderId { get; set; }
         public int InternalId { get; set; }
         public DateTime StartTime { get; set; }
         public RecurrencePattern RecurrencePattern { get; set; }
     }
 
     /// <summary>
-    /// Definerer ulike mønstre for gjentakelse av hentinger.
+    /// Definerer ulike mønstre for gjentakelse av varemottak.
     /// </summary>
     public enum RecurrencePattern
     {
-        Daily,   // Henting skjer hver dag.
-        Weekly   // Henting skjer ukentlig.
+        Daily,   // Mottak skjer hver dag.
+        Weekly   // Mottak skjer ukentlig.
     }
 }
